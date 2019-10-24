370394460e1f1136dcf2ae51790e3bf931f2c783
PieterKnops <pieterknops@hotmail.com>
2019-10-24 11:53:25 +0200

    first version

--- model2.R
+++ model2.R
@@ -2,13 +2,14 @@ model <- function(t, state, parms) {
   with(as.list(c(state,parms)), {
     dWILG <- r*WILG*(1-WILG/K) - p * WILG * ELK; #effect wolven op eten??
     dELK <- r*ELK*(1-ELK/K) - ELK*WOLF/(1+WOLF);
-    dWOLF <- c * E*WOLF/(1+WOLF) - WOLFd*WOLF;
+    dWOLF <- c * ELK*WOLF/(1+WOLF) - WOLFd*WOLF;
     return(list(c(dWILG, dELK, dWOLF)))  
   })
 }  
 
-p <- c(r=1,K=10,WOLFd=0.13,c=0.1,WILGd=0.03,p=0.1)
-s <- c(WILG=10,ELK=10,WOLF=1)
+p <- c(r=1,K=10,WOLFd=0.13,c=0.1,WILGd=0.03,p=10)
+s <- c(WILG=10,ELK=10,WOLF=0)
 run()
+
 plane(xmax=20,ymax=10,eps=-0.01);f<-run(traject=T)
 newton(c(WILG=10,ELK=3.605551,WOLF=0),plot=T)
