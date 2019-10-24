1bd3667474120f25207ac570d926eee0f596b16d
PieterKnops <pieterknops@hotmail.com>
2019-10-24 11:54:03 +0200

    wij

--- model2.R
+++ model2.R
@@ -1,14 +1,14 @@
 model <- function(t, state, parms) {
   with(as.list(c(state,parms)), {
-    dWILG <- r*WILG*(1-WILG/K) - p * WILG * ELK; #effect wolven op eten??
+    dWILG <- r*WILG*(1-WILG/K) - p * WILG * ELK; #effect wolven op eten?
     dELK <- r*ELK*(1-ELK/K) - ELK*WOLF/(1+WOLF);
     dWOLF <- c * ELK*WOLF/(1+WOLF) - WOLFd*WOLF;
     return(list(c(dWILG, dELK, dWOLF)))  
   })
 }  
 
-p <- c(r=1,K=10,WOLFd=0.13,c=0.1,WILGd=0.03,p=10)
-s <- c(WILG=10,ELK=10,WOLF=0)
+p <- c(r=1,K=10,WOLFd=0.13,c=0.1,WILGd=0.03,p=0.02)
+s <- c(WILG=10,ELK=10,WOLF=1)
 run()
 
 plane(xmax=20,ymax=10,eps=-0.01);f<-run(traject=T)
