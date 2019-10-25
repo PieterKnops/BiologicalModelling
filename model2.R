1fc9a96d58c9a7e114534f73e74443a90bf27c86
PieterKnops <pieterknops@hotmail.com>
2019-10-25 11:08:12 +0200

    added life expectancies (death terms)

--- model2.R
+++ model2.R
@@ -8,9 +8,13 @@ model <- function(t, state, parms) {
   })
 }  
 
-p <- c(rWILG=1,rELK=0.8,K=10,deathWOLF=0.13,c=0.1,d=0.02,deathBEAR=0.05,deathWILG=0.03,p=0.02)
+p <- c(rWILG=1,rELK=0.8,K=10,c=0.1,d=0.02,deathWOLF=0.13,deathBEAR=0.042,deathWILG=0.022,p=0.02)
 s <- c(WILG=10,ELK=10,BEAR=1,WOLF=1)
 run(tmax=1000)
 
 plane(xmax=20,ymax=10,eps=-0.01);f<-run(traject=T)
 newton(c(WILG=10,ELK=3.605551,WOLF=0),plot=T)
+
+# life expectancy wolves: https://www.yellowstonepark.com/things-to-do/wildlife/wolves 
+# life expectancy black bears, grizzly bear respectively: https://www.nps.gov/yell/learn/nature/black-bear.htm https://www.nps.gov/yell/learn/yellowstone-grizzly-bear-facts.htm
+# life expectancy willow: https://nl.wikipedia.org/wiki/Wilg#Knotwilg <- TODO not sure about this one
