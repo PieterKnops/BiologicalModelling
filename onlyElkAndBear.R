87a357b5ea2e5017f9cb472754e0122db50da36d
PieterKnops <pieterknops@hotmail.com>
2019-10-26 20:49:11 +0200

    tryouts

--- onlyElkAndBear.R
+++ onlyElkAndBear.R
@@ -1,12 +1,13 @@
 model <- function(t, state, parms) {
   with(as.list(c(state,parms)), {
-    dE <- r*E*(1-E/K) - e*B*E/(1+E);
-    dB <- c * B*E/(1+E) - deathB*B;
-    return(list(c(dE, dB)))  
+    dELK <- r*ELK*(1-ELK/K) - e*B*ELK/(1+ELK);
+    dB <- c * B*ELK/(1+ELK) - deathB*B;
+    return(list(c(dELK, dB)))  
   }) 
 }  
 
 p <- c(r=1,e=80,K=3,c=0.07,deathB=1/24)
-s <- c(E=1.5,B=0.015)
+s <- c(ELK=1.5,B=0.015)
 plane(xmax=3,ymax=0.02,eps=-0.001);f<-run(traject=T)
-newton(c(E=1,B=1),plot=T)
+newton(c(ELK=1,B=1),plot=T)
+
