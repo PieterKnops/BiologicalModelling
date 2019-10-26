43eac8e9a3e5702e7f0ea60fa2a847725b4cf427
PieterKnops <pieterknops@hotmail.com>
2019-10-26 18:14:07 +0200

    stable!

--- onlyElkAndBear.R
+++ onlyElkAndBear.R
@@ -1,11 +1,12 @@
 model <- function(t, state, parms) {
   with(as.list(c(state,parms)), {
-    dELK  <- rELK*ELK*(1-ELK/K)  - e*ELK*BEAR/(h + ELK) - deathELK * ELK; # verzadging helpen? Juvenile? Effect on 1?
-    dBEAR <- c*e*ELK*BEAR/(h+ELK) - deathBEAR*BEAR; # Mass action? Bears and Wolves birth rates were saturation of themselves instead of elk. I fixed this cause Im smart.
-    return(list(c(dELK, dBEAR)))  # De conversiefactor van de beren was hetzelfde als hun death rate, heb ik ook veranderd.
-  })
+    dE <- r*E*(1-E/K) - e*B*E/(1+E);
+    dB <- c * B*E/(1+E) - deathB*B;
+    return(list(c(dE, dB)))  
+  }) 
 }  
 
-p <- c(rELK=1.0,K=2,c=0.02,e=9,h=4.5,deathELK=0.05,deathBEAR=0.042)
-s <- c(ELK=1.5,BEAR=0.02)
-run(tmax=1000,ymax=10)
+p <- c(r=1,e=80,K=3,c=0.07,deathB=1/24)
+s <- c(E=1.5,B=0.015)
+plane(xmax=3,ymax=0.02,eps=-0.001);f<-run(traject=T)
+newton(c(E=1,B=1),plot=T)
